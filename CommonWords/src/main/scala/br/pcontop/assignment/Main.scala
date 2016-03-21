419ee0d34d3f4240a25251a2df6c77ce575e1fa0
pcontop <pcontopoulos@gmail.com>
2016-03-21 01:39:01 -0300

    Added to Intellij. Basic cleanup. Will try now to resolve 'more than one scala library' issue.

--- CommonWords/src/main/scala/br/pcontop/assignment/Main.scala
+++ CommonWords/src/main/scala/br/pcontop/assignment/Main.scala
@@ -1,10 +1,9 @@
 package br.pcontop.assignment
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.jsoup.Jsoup
 import java.io._
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.jsoup.Jsoup
 
 object Main {
 
@@ -28,23 +27,23 @@ object Main {
 		sc.hadoopConfiguration.set("mapreduce.input.fileinputformat.input.dir.recursive","true")
 
 		//Book a
-		var words1 = sc.wholeTextFiles(pathToBook1 + "/**").map{case(_,y) => Jsoup.parse(y).text()}.flatMap{_.split(" ")}
+		val words1 = sc.wholeTextFiles(pathToBook1 + "/**").map{case(_,y) => Jsoup.parse(y).text()}.flatMap{_.split(" ")}
 
 		//words.count()
 
-		var wordCount1 = words1.map{x => (x,1)}.reduceByKey{(x,y) => x + y}
+		val wordCount1 = words1.map{x => (x,1)}.reduceByKey{(x,y) => x + y}
 
 		//Book b
-		var words2 = sc.wholeTextFiles(pathToBook2 + "/**").map{case(_,y) => Jsoup.parse(y).text()}.flatMap{_.split(" ")}
+		val words2 = sc.wholeTextFiles(pathToBook2 + "/**").map{case(_,y) => Jsoup.parse(y).text()}.flatMap{_.split(" ")}
 
-		var wordCount2 = words2.map{x => (x,1)}.reduceByKey{(x,y) => x + y}
+		val wordCount2 = words2.map{x => (x,1)}.reduceByKey{(x,y) => x + y}
 
-		var joinBooks = wordCount1.join(wordCount2)
+		val joinBooks = wordCount1.join(wordCount2)
 
-		var joinBooksSum = joinBooks.map{case(word, (one, two)) => (word, one + two)}
+		val joinBooksSum = joinBooks.map{case(word, (one, two)) => (word, one + two)}
 
 		//Final Result
-		var result = joinBooksSum.map(a => a.swap).sortByKey(false).map{a => a._2}.take(1500)
+		val result = joinBooksSum.map(a => a.swap).sortByKey(false).map { a => a._2 }.take(1500)
 
 		//Printing to file.
 
